5e572be86c6d5004a02270de471cb6cc65cfc761
tryard90-eng <tryard90@gmail.com>
2026-05-10 09:46:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Dosyaların bulunduğu klasörü tanıt
-app.use(express.static(path.join(__dirname, '.')));
+// Dosyaları kök dizinden çekmesini zorunlu kılıyoruz
+app.use(express.static(__dirname));
 
-// Ana sayfa isteği geldiğinde index.html'i gönder
+// Her türlü istekte index.html'i fırlat
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
-// Port ayarı (Render için kritik)
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Sunucu ${PORT} portunda canavar gibi çalışıyor!`);
+    console.log(`Site hazır: ${PORT}`);
 });
